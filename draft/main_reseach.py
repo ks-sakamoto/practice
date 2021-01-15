4dab36bd246e4bc3ff377fe02e5e005b9e0ed89c
Kazuya Sakamoto <ks.netgit@gmail.com>
2021-01-15 19:31:46 +0900

    researchのmainの更新に伴う更新

--- draft/main_reseach.py
+++ draft/main_reseach.py
@@ -7,7 +7,7 @@ import gc
 import _thread
 
 
-def main(path):
+def main(path, main_lock):
     print('===============')
     gc.collect()
     print('Func run free: {} allocated: {}'.format(
@@ -21,10 +21,12 @@ def main(path):
     ext = alz.Extract(path)
     # ファクトの抽出
     fact_pro, fact_ini = ext.fact()
+    agent_name = ext.agentname()
 
     # micropython.mem_info()
 
-    print("fact_pro: {0}, \nfact_ini: {1}".format(fact_pro, fact_ini))
+    print("fact_pro: {0}, \nfact_ini: {1}\nagent_name: {2}".format(
+        fact_pro, fact_ini, agent_name))
     print('===============')
     gc.collect()
     print('Func run free: {} allocated: {}'.format(
@@ -47,10 +49,9 @@ def main(path):
     for s in actions:
         # オブジェクトを抽出
         content = extractOVAtoDict(s, vars)
-        print(content)
         if content['obj'] == 'send':
             # exit()
-            lpwa(uart, content)
+            lpwa(uart, content, main_lock)
 
             '''
             アクション実行, ここでfact_iniが書き換わる可能性あり。
@@ -75,37 +76,56 @@ def main(path):
     gc.collect()
     print('Func run free: {} allocated: {}'.format(
         gc.mem_free(), gc.mem_alloc()))
+
     while True:
         utime.sleep(3)
         print('========mainだよ=======')
 
 
-def lpwa(uart, content):
+def read_status_block(main_lock):
+    while(1):
+        global status_que
+        with main_lock:
+            if len(status_que) > 0:
+                return status_que.pop(0)
+            else:
+                pass
 
-    print('----lpwa----')
+        utime.sleep(3)
+
+
+def lpwa(uart, content, main_lock):
+    global status_que
+    global receive_que
+
+    print('---------')
+    # utime.sleep(3)
     # uart.write(b'RDID\r\n')
-    utime.sleep(3)
-    # msg_bytes = uart.readline()
-    # print(str(msg_bytes, 'UTF-8'))
+    # print('RDIDは')
+    # status = read_status_block()
+    # print(status)
 
+    print('---------')
     # uart.write(b'ECIO\r\n')
-    utime.sleep(2)
-    # msg_bytes = uart.readline()
-    # print(str(msg_bytes, 'UTF-8'))
-#     string = 'Hello World!\r\n'
-
+    # print('ECIOは')
+    # status = read_status_block(main_lock)
+    # print(str(status, 'UTF-8'))
+    print(content)
     for k, v in content.items():
+        utime.sleep(3)
+        print('いまは key:{}, value:{}'.format(k, v))
         if k == 'obj':
             pass
         else:
             if len('{},{}'.format(k, v)) < 32:
                 while(1):
                     print('送信文字列 key:{}, value:{}'.format(k, v))
-                    # uart.write(b'TXDA {}'.format(k, v, '\r\n'))
-                    utime.sleep(3)
                     break
-                    # print(str(msg_bytes, 'UTF-8'))
-                    # if msg_bytes == b'OK\r\n':
+                    # uart.write(b'TXDA {}{}{}'.format(k, v, '\r\n'))
+                    # # utime.sleep(3)
+                    # status = read_status_block(main_lock)
+                    # print(status)
+                    # if status == b'OK\r\n':
                     #     print('送信完了')
                     #     break
                     # else:
@@ -116,16 +136,79 @@ def lpwa(uart, content):
                     while(1):
                         print('送信文字列: {}{}'.format(
                             '{},{}'.format(k, v)[32*i:32*(i+1)], '\r\n'))
+                        break
                         # uart.write(b'TXDA {}{}'.format(
                         #     '{},{}'.format(k, v)[32*i:32*(i+1)], '\r\n'))
-                        utime.sleep(3)
-                        break
-                        # print(str(msg_bytes, 'UTF-8'))
-                        # if msg_bytes == b'OK\r\n':
+                        # # utime.sleep(3)
+                        # status = read_status_block(main_lock)
+                        # print(str(status, 'UTF-8'))
+                        # if status == b'OK\r\n':
                         #     print('送信完了')
                         #     break
                         # else:
                         #     print('送信失敗')
+
+
+'''
+    for strs in joken:
+        for string in strs:
+            if(len(string) > 32):
+                num = (len(string)//32) + 1
+                for i in range(num):
+                    while(1):
+                        print('送信文字列: {}{}'.format(
+                            string[32*i:32*(i+1)], '\r\n'))
+                        uart.write(b'TXDA {}{}'.format(
+                            string[32*i:32*(i+1)], '\r\n'))
+                        utime.sleep(3)
+                        msg_bytes = uart.readline()
+                        print(str(msg_bytes, 'UTF-8'))
+                        if msg_bytes == b'OK\r\n':
+                            print('送信完了')
+                            break
+                        else:
+                            print('送信失敗')
+            else:
+                while(1):
+                    print('送信文字列: {}{}'.format(string, '\r\n'))
+                    uart.write(b'TXDA {}{}'.format(string, '\r\n'))
+                    utime.sleep(3)
+                    print(str(msg_bytes, 'UTF-8'))
+                    if msg_bytes == b'OK\r\n':
+                        print('送信完了')
+                        break
+                    else:
+                        print('送信失敗')
+    # hensyu = {'?from': 'Japannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
+    #           '?content': 'question'}
+    for k, v in hensyu.items():
+        if len('{},{}'.format(k, v)) < 32:
+            while(1):
+                print('送信文字列 key:{}, value:{}'.format(k, v))
+                uart.write(b'TXDA {}'.format(k, v, '\r\n'))
+                utime.sleep(3)
+                print(str(msg_bytes, 'UTF-8'))
+                if msg_bytes == b'OK\r\n':
+                    print('送信完了')
+                    break
+                else:
+                    print('送信失敗')
+        else:
+            num = (len('{},{}'.format(k, v))//32) + 1
+            for i in range(num):
+                while(1):
+                    print('送信文字列: {}{}'.format(
+                        '{},{}'.format(k, v)[32*i:32*(i+1)], '\r\n'))
+                    uart.write(b'TXDA {}{}'.format(
+                        '{},{}'.format(k, v)[32*i:32*(i+1)], '\r\n'))
+                    utime.sleep(3)
+                    print(str(msg_bytes, 'UTF-8'))
+                    if msg_bytes == b'OK\r\n':
+                        print('送信完了')
+                        break
+                    else:
+                        print('送信失敗')
+'''
 
 
 def extractOVAtoDict(s, vars):
@@ -159,7 +242,6 @@ def extractOVAtoDict(s, vars):
         s = re.sub('\?\w+:\w+', '', s, 1)
         count += 1
         dict_ova[count] = s
-        count += 1
     # modify以外の場合
     else:
         regex2 = re.compile('^(:\w+)\s+([a-zA-Z0-9?_()"\'@ -]+)')
@@ -222,42 +304,65 @@ def aciton_modify(modi_varattr, new_value, fact_ini, vars):
         # 新しい値に置き換える
         new_fact = re.sub(ori_value, new_value, ori_fact)
     else:
+        new_fact = ''
         print("Can't modify")
 
     fact_ini.remove('({})'.format(ori_fact))
     fact_ini.append('({})'.format(new_fact))
 
 
-def check(agent_name):
-    print('hello')
+def check(agent_name, main_lock):
+    global status_que
+    global receive_que
+
     # uart = UART(2, 19200)  # 与えたボーレートで初期化
     # uart.init(baudrate=19200, bits=8, parity=None,
     #           stop=1, rx=16, tx=17)  # 与えたパラメータで初期化
-    # while True:
-    utime.sleep(3)
-    print('===check===')
-    # msg_bytes = uart.readline()
-    # print(msg_bytes)
-    # if msg_bytes is not None:
-    #     print('受信しました')
-    # if agent_name ==  :toで抜き出した名前と一致すれば。それでいいのか？
 
-    # print(str(msg_bytes, 'UTF-8'))
+    while True:
+        utime.sleep(5)
+        print('===check===')
+        # msg_bytes = uart.readline()
+        # print('checkでのreadは')
+        # print(msg_bytes)
+        # if msg_bytes is not None:
+        #     if msg_bytes == b'OK\r\n' or msg_bytes == b'NG\r\n':
+        #         with main_lock:
+        #             print('status queにappendしたよ')
+        #             status_que.append(msg_bytes)
+        #     else:
+        #         with main_lock:
+        #             receive_que.append(msg_bytes)
+        #             print('メッセージを受信しました')
+        #             message = receive_que.pop(0)
+        #             print(message)
+
+        #             print(agent_name)
+        #             if agent_name == 'Sample2':
+        #                 print('agent nameはSample2')
 
 
 if __name__ == '__main__':
     path = 'Sample2.dash'
-    agent_name = path[:-5]
-    # main(path)
-    # _thread.stack_size(40000)
+    # agent_name = path[:-5]
+    with open('Sample2.dash', encoding='utf-8') as f:
+        data = f.readline()
+        # regex = re.split('\(agent ', data)[1]
+    agent_name = data[7:-1]
+    print('agent_name')
+    print(agent_name)
+    utime.sleep(1)
+
+    receive_que = []
+    status_que = []
+
     try:
-        _thread.start_new_thread(check, (agent_name,))
+        main_lock = _thread.allocate_lock()
+        _thread.start_new_thread(check, (agent_name, main_lock, ))
         # _thread.stack_size(10000)
-        _thread.start_new_thread(main, (path,))
+        _thread.start_new_thread(main, (path, main_lock,))
     except Exception as e:
         print(e)
-    # except:
-    #     print("Error: unable to start thread")
 
 
 '''
